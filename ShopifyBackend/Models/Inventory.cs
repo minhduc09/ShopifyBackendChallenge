348894c9e2d462be7baf109d8121a93426c2511a
Duc Hoang <mducdev@gmail.com>
2022-05-22 17:52:15 -0400

    Init

---
+++ ShopifyBackend/Models/Inventory.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ShopifyBackend.Models
+{
+    public class Inventory
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Required(ErrorMessage = "Please enter Name")]
+        public string Name { get; set; }    
+
+        [Required(ErrorMessage ="Please enter Quantity")]
+        public int Quantity { get; set; }
+    }
+}
---
+++ ShopifyBackend/Models/InventoryContext.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace ShopifyBackend.Models
+{
+    public class InventoryContext : DbContext
+    {
+        public InventoryContext() { }
+
+        public InventoryContext(DbContextOptions<InventoryContext> options) : base(options) 
+        { 
+
+        }
+
+        public DbSet<Inventory> Inventories { get; set; }
+
+        public DbSet<Location> Locations { get; set; }
+    }
+}
